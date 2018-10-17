bf8fe916f80f96653069643ccc84547a9e68931d
yu.xie <471519362@qq.com>
2018-10-17 17:13:23 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Header from './Header'
+import Content from './Content'
+
+import createStore from './store'
+import themeReducer from './reducer'
+
+const store = createStore(themeReducer)
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Header />
+        <Content />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/react-redux.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export const connect = (mapStateToProps, mapDispatchToProps) => (WrappedComponnet) => {
+    class Connect extends Component {
+        static contextTypes = {
+            store: PropTypes.object
+        }
+
+        constructor() {
+            super()
+            this.state = { allProps: {} }
+        }
+
+        componentWillMount() {
+            const { store } = this.context
+            this.updateProps()
+            store.subscribe(() => this.updateProps())
+        }
+
+        updateProps() {
+            const { store } = this.context
+            let stateProps = mapStateToProps(store.getState(), this.props)
+            let dispatchProps = mapDispatchToProps ? mapDispatchToProps(store.dispatch, this.props) : {}
+            this.setState({
+                allProps: {
+                    ...stateProps,
+                    ...dispatchProps,
+                    ...this.props
+                }
+            })
+        }
+
+        render() {
+            return <WrappedComponnet {...this.state.allProps} />
+        }
+    }
+
+    return Connect
+}
+
+export class Provider extends Component {
+    static propTypes = {
+        store: PropTypes.object,
+        children: PropTypes.any
+    }
+
+    static childContextTypes = {
+        store: PropTypes.object
+    }
+
+    getChildContext() {
+        return {
+            store: this.props.store
+        }
+    }
+
+    render() {
+        return (
+            <div>{this.props.children}</div>
+        )
+    }
+}
